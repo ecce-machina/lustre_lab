c7c6ae91712584c4268ced98f7d1137aebf5e33b
Matt Dube <mattdube1837@gmail.com>
2026-05-28 12:22:13 -0400

    removing ansible functionality for now, will use simple shell script with gcloud for the moment

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -34,16 +34,3 @@ output "oss_names" {
   value = google_compute_instance.oss[*].name
 }
 
-output "ansible_inventory" {
-    value = templatefile("${path.module}/inventory.tmpl", {
-        mds_name    = google_compute_instance.mds.name
-        mds_ip      = google_compute_instance.mds.network_interface[0].network_ip
-        oss_names   = google_compute_instance.oss[*].name
-        oss_ips     = google_compute_instance.oss[*].network_interface[0].network_ip
-        client_names = google_compute_instance.client[*].name
-        client_ips   = google_compute_instance.client[*].network_interface[0].network_ip
-        zone        = var.zone
-        project_id  = var.project_id
-        ansible_user = var.ansible_user
-    })
-}
